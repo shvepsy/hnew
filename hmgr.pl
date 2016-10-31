0b8f37da05d9195e73fbec92bc8b811d8aa14c1e
Shvepsy.work <exr2@mail.ru>
2016-10-31 19:26:04 +0300

    Init

---
+++ hmgr.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+
+use strict; 
+use warnings;
+
+#use JSON;
+use POSIX qw(strftime);
+#use JSON::Parse 'valid_json';
+use JSON::Parse ':all';
+use Data::Dumper;
+
+sub event {
+	my $msg = shift;
+	if (open (LOG, ">>./mgmt.log") ) { 
+	my $time = strftime "%e %b %H:%M:%S %Y", localtime;
+		print LOG "$time: $msg\n";
+		close LOG; 
+		print "$msg\n"; #
+		exit 1 if ($msg =~ /ERR/ );
+		return 0;
+	}
+	else {
+		print "Can't open logfile";
+		return 1;
+	}
+}
+
+# Read JOSN conf
+event("Start");
+my $jsonr;
+open (FH, "<./test.json") or event("ERR:Can't open json\n");
+while (<FH>) { $jsonr .= $_ } ;
+close FH; 
+event("ERR:Invalid JSON") unless (valid_json ($jsonr));
+
+
+my $jsonp =  parse_json ( $jsonr );  
+
+print Dumper($jsonp);
+#print $jsonr; 
+#print $jsonp; 
+event("End");
+exit 0;
+
+
